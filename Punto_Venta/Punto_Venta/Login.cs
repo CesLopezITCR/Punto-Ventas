083d67f32fb4fa77a76ffe2aeb009469074bc28c
Cesar Lopez <cesar81095cr@gmail.com>
2017-06-14 12:29:04 -0600

    Creación de lla conexión a base de datos

--- Punto_Venta/Punto_Venta/Login.cs
+++ Punto_Venta/Punto_Venta/Login.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace Punto_Venta
 {
@@ -13,6 +14,10 @@ namespace Punto_Venta
 
             Console.WriteLine(usuario);
             Console.WriteLine(contra);
+
+            MySqlConnection conectar = new MySqlConnection("server=127.0.0.1; database=pos; Uid=root; pwd=marces85;");
+
+            conectar.Open();
         }
     }
 
