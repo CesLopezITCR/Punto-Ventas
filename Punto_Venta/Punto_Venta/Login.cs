1615bde2120e51f1150ca4bde9e287260072dc0b
Cesar Lopez <cesar81095cr@gmail.com>
2017-06-14 16:18:56 -0600

    Validaciones del login con la db

--- Punto_Venta/Punto_Venta/Login.cs
+++ Punto_Venta/Punto_Venta/Login.cs
@@ -12,12 +12,44 @@ namespace Punto_Venta
 
         public void iniciar(String usuario, String contra) {
 
-            Console.WriteLine(usuario);
-            Console.WriteLine(contra);
+            List<string> nombre = new List<string>();
+            List<string> pass = new List<string>(); ;
 
-            MySqlConnection conectar = new MySqlConnection("server=127.0.0.1; database=pos; Uid=root; pwd=marces85;");
+            string conexion = "server=localhost; database=pos; Uid=root; pwd=marces85;";
+            MySqlConnection conn = new MySqlConnection(conexion);
+            conn.Open();
+            
+            string query = "select * from pos.usuarios";
 
-            conectar.Open();
+            MySqlCommand mycomand = new MySqlCommand(query, conn);
+            MySqlDataReader myreader = mycomand.ExecuteReader();
+            
+
+            while (myreader.Read()){
+                
+                nombre.Add(myreader["nombre"].ToString());
+                pass.Add(myreader["contra"].ToString());
+            }
+            conn.Close();
+
+            //nombre.ForEach(Console.WriteLine);
+            //pass.ForEach(Console.WriteLine);
+
+            for (int i = 0; i < nombre.Count; i++) {
+
+                if (usuario.Equals(nombre[i]) && contra.Equals(pass[i]))
+                {
+                    Console.WriteLine("correcto");
+                    System.Windows.MessageBox.Show("Bienvenido");
+                }
+                else {
+                    if(i < nombre.Count-1) {
+
+                        System.Windows.MessageBox.Show("Usuario y/o Contraseña " +
+                        "Inválidos");
+                    }
+                }
+            }
         }
     }
 
